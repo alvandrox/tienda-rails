a7b544ade7e3074976c85ef213a0a96af1040712
alvandrox <alvandrox@gmail.com>
2015-07-31 18:31:38 -0300

    cambiado de usr a usuario (tabla)

--- db/migrate/20150731212939_devise_create_usuarios.rb
+++ db/migrate/20150731212939_devise_create_usuarios.rb
@@ -1,6 +1,6 @@
-class DeviseCreateUseres < ActiveRecord::Migration
+class DeviseCreateUsuarios < ActiveRecord::Migration
   def change
-    create_table(:useres) do |t|
+    create_table(:usuarios) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -34,9 +34,9 @@ class DeviseCreateUseres < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    add_index :useres, :email,                unique: true
-    add_index :useres, :reset_password_token, unique: true
-    # add_index :useres, :confirmation_token,   unique: true
-    # add_index :useres, :unlock_token,         unique: true
+    add_index :usuarios, :email,                unique: true
+    add_index :usuarios, :reset_password_token, unique: true
+    # add_index :usuarios, :confirmation_token,   unique: true
+    # add_index :usuarios, :unlock_token,         unique: true
   end
 end
